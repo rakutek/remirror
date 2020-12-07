275bbdbc4fd29686eeff8556cf709542825546a6
mp3 <im.in.a.coma.xxx@gmail.com>
2020-12-07 16:44:04 +0900

    getting started

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,39 @@
+import React from 'react'
+import { BoldExtension } from 'remirror/extension/bold'
+import { RemirrorProvider, useManager, useRemirror } from 'remirror/react'
+
+const Button = () => {
+  const { active, commands } = useRemirror({ autoUpdate: true })
+
+  return (
+    <>
+      <button
+        onClick={() => commands.toggleBold()}
+        style={{ fontWeight: active.bold() ? 'bold' : undefined }}
+      >
+        Bold
+      </button>
+    </>
+  )
+}
+
+const Editor = () => {
+  const { getRootProps } = useRemirror()
+
+  return <div {...getRootProps()} />
+}
+
+const EditorWrapper = () => {
+  const manager = useManager(() => [new BoldExtension()])
+
+  return (
+    <RemirrorProvider manager={manager}>
+      <Editor />
+      <Button />
+    </RemirrorProvider>
+  )
+}
+
 export default function Top() {
-  return <div>Hello</div>
+  return <EditorWrapper />
 }
